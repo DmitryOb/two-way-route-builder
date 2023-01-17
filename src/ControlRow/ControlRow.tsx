a357f77e31ca0764b5f07e5e2014a0e65c005b38
DmitryOb <breakdance101@yandex.ru>
2023-01-17 10:38:49 +0300

    from Loo

--- src/ControlRow/ControlRow.tsx
+++ src/ControlRow/ControlRow.tsx
@@ -6,6 +6,7 @@ import {appStore, EnumPoints} from "../App";
 import "./ControlRow.css";
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from "@mui/material";
 
 interface ControlRowProps {
 }
@@ -14,6 +15,7 @@ const ControlRow: FC<ControlRowProps> = () => {
   const date = appStore((state) => state.date); // 'YYYY-MM-DD'
   const setDate = appStore((state) => state.setDate);
   const setGoesTo = appStore((state) => state.setGoesTo);
+  const goesFrom = appStore((state) => state.goesFrom);
   const setGoesFrom = appStore((state) => state.setGoesFrom);
   const setPossible = appStore((state) => state.setPossible);
   const filterByPossible = appStore((state) => state.filterByPossible);
@@ -27,32 +29,57 @@ const ControlRow: FC<ControlRowProps> = () => {
     setPossible(event.target.checked)
   };
 
+  const getDateDescription = (date: string): { visible: boolean, name: string } => {
+    switch (date) {
+      case moment().format('YYYY-MM-DD'):
+        return {name: 'Сегодня', visible: true}
+      case moment().add(1, 'd').format('YYYY-MM-DD'):
+        return {name: 'Завтра', visible: true}
+      default:
+        return {name: 'None', visible: false}
+    }
+  }
+
   return (
     <div className={'control-row'}>
       <div className={'control-column-first'}>
-        {isShowBackButton() &&
-          <button onClick={() => {
-            const dayBefore = moment(date).subtract(1, 'd').format('YYYY-MM-DD');
-            setDate(dayBefore);
-          }}>
+        <div className={'date'}>
+          <button style={{visibility: isShowBackButton() ? 'visible' : 'hidden'}}
+                  onClick={() => {
+                    const dayBefore = moment(date).subtract(1, 'd').format('YYYY-MM-DD');
+                    setDate(dayBefore);
+                  }}
+          >
             <ArrowBackIosNewIcon fontSize={"small"}/>
           </button>
-        }
-        <div>
-          <span>{date}</span>
-          <br/>
-          {date === moment().format('YYYY-MM-DD') &&
-            <span>Сегодня</span>
-          }
+          <div>
+            <span>{date}</span>
+            <br/>
+            <span style={{visibility: getDateDescription(date).visible ? 'visible' : 'hidden'}}>
+              {getDateDescription(date).name}
+            </span>
+          </div>
+          <button onClick={() => setDate(moment(date).add(1, 'd').format('YYYY-MM-DD'))}>
+            <ArrowForwardIosIcon fontSize={"small"}/>
+          </button>
         </div>
-        <button onClick={() => setDate(moment(date).add(1, 'd').format('YYYY-MM-DD'))}>
-          <ArrowForwardIosIcon fontSize={"small"}/>
-        </button>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label" style={{background: 'white'}}>Откуда</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={goesFrom}
+            label="Age"
+            onChange={(event: SelectChangeEvent) => setGoesFrom(event.target.value as EnumPoints)}
+            autoWidth
+          >
+            <MenuItem value={EnumPoints.SOVHOZ}>Совхоз</MenuItem>
+            <MenuItem value={EnumPoints.LOO}>Лоо</MenuItem>
+          </Select>
+        </FormControl>
       </div>
+
       <div className={'control-column-second'}>
-        <button onClick={() => setGoesFrom(EnumPoints.LOO)}>
-          из лоо!
-        </button>
         <button onClick={() => setGoesTo(EnumPoints.IMERITIN_RESORT)}>
           на курорт!
         </button>
